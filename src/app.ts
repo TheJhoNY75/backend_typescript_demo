f2f0f560cca45607e8cd0a20fd3002741909487f
Jhonatan Ancona <jhonatan_ancona@softwarenextdoor.com>
2023-01-05 18:15:07 -0500

    update de comentario para test

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import morgan from 'morgan';
 import indexRoutes from './routes/index.routes';
 import postRoutes from './routes/post.routes';
 
+//primer clase
+
 export class App{
   
   private app: Application;
