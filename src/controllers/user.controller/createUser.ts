95c327f1d7d2d88236ac2a5fe0d54a5bd93eb872
Jhonatan Ancona <jhonatan_ancona@softwarenextdoor.com>
2023-01-08 16:09:33 -0500

    update de validacion de dupliacion de correo al registrase

--- src/controllers/user.controller/createUser.ts
+++ src/controllers/user.controller/createUser.ts
@@ -4,13 +4,20 @@ import { v4 as uuid } from "uuid";
 import { User } from "../../interfaces/User";
 import { connect } from "../../database";
 
-export async function createUser( req: Request, res: Response): Promise<Response> {
+export async function createUser( req: Request, res: Response) {
   const { email, first_name, last_name, password }: User = req.body;  
   const id = uuid();
   const hashedPassword = await bcrypt.hash( password, 10);
   const conn = await connect();
-  await conn.query("INSERT INTO users SET ?", [
-    { id, email, first_name, last_name, password: hashedPassword, role_id: 1 },
-  ]);
-  return res.json({ id, email, first_name, last_name });
+  try{
+    await conn.query("INSERT INTO users SET ?", [
+      { id, email, first_name, last_name, password: hashedPassword, role_id: 1 },
+    ]);
+    return res.json({ id, email, first_name, last_name });
+  }catch(err: any){
+    console.log(err);
+    if(err.code === "ER_DUP_ENTRY")
+      return res.json({ message: "Email already exists" });
+    return res.json({ message: "Something went wrong" });
+  }
 }
